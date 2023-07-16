2b5c8bfd9899bcc68c9d67970a51bd7a3128f7d0
Damir Alimbekov <damir.alimbekov.2002@gmail.com>
2023-07-16 17:00:10 +0500

    Реализован поиск по JDoc

--- bot_plugins.py
+++ bot_plugins.py
@@ -1,13 +1,19 @@
 from mmpy_bot import Plugin, Message
 from mmpy_bot import listen_to
 from searching.searchers import RTDSearcher, MicroimpulsSearcher, GithubPagesSearcher
+from searching.searchers import JsDocSearcher, MattermostHistorySearcher
 import json
 
 try:
     with open('config.json', "r") as config_file:
-        RTD_TOKEN = json.loads(config_file.read())["readthedocs_token"]
+        config = json.loads(config_file.read())
+        RTD_TOKEN = config["readthedocs_token"]
+        MM_TOKEN = config["mattermost_token"]
 except IOError as e:
     print(f'Unable to read config: {e}')
+    exit(1)
+except KeyError as e:
+    print(f'Cannot find value in cconfig: {e}')
     exit(1)
 
 class GreetPlugin(Plugin):
@@ -25,9 +31,19 @@ class SearchPlugin(Plugin):
             GithubPagesSearcher("https://microimpuls.github.io/smarty-billing-api-docs/"),
             GithubPagesSearcher("https://microimpuls.github.io/smarty-viewstats-api-docs/"),
             GithubPagesSearcher("https://microimpuls.github.io/smarty-device-monitoring-api-docs/"),
-            GithubPagesSearcher("https://microimpuls.github.io/smarty-content-api-docs/")
+            GithubPagesSearcher("https://microimpuls.github.io/smarty-content-api-docs/"),
+            JsDocSearcher("https://microimpuls.github.io/justify-impuls-screens-jsdoc/"),
+            JsDocSearcher("https://microimpuls.github.io/justify-engine-main-jsdoc/"),
+            JsDocSearcher("https://microimpuls.github.io/justify-impuls-main-jsdoc/"),
+            MattermostHistorySearcher(
+                MM_TOKEN, "https://bottestformicro.cloud.mattermost.com",
+                "u3nwyiepx78kbkdtd58it1dp3o")
         ]
 
+    @listen_to("^alive$")
+    async def alive(self, message: Message):
+        self.driver.reply_to(message, "I'm alive!")
+    
     @listen_to("^wiki (.*)$")
     async def search(self, message: Message, expr: str):
         pass
@@ -43,9 +59,16 @@ class SearchPlugin(Plugin):
         self.driver.reply_to(message, f"#### Results:\n{ result }")
 
     @listen_to("^gitdev (.*)$")
-    async def search_in_github_pages(self, message: Message, expr: str):
+    async def search_in_gitdocs(self, message: Message, expr: str):
         self.driver.reply_to(message, "#### Results:")
-        for searcher in self.searchers[2:]:
+        for searcher in self.searchers[2:-1]:
             result = searcher.search(expr)
             for row in result.rows():
                 self.driver.reply_to(message, f"{row}")
+
+    @listen_to("^history (.*)$")
+    async def search_in_mattermost_history(self, message: Message, expr: str):
+        result = self.searchers[-1].search(expr)
+        self.driver.reply_to(message, f"#### Results:")
+        for row in result.rows():
+            self.driver.reply_to(message, row)
--- searching/searchers.py
+++ searching/searchers.py
@@ -1,9 +1,10 @@
 from . import Searcher, SearchResult
 from markdownify import markdownify
 from selenium import webdriver
-import requests
-import re
-import bs4
+import requests, re, bs4
+from operator import itemgetter
+from datetime import datetime
+from selenium.webdriver.common.by import By
 
 class RTDSearcher(Searcher):
     '''Поиск по документации на портале Read The Docs'''
@@ -106,3 +107,78 @@ class GithubPagesSearcher(Searcher):
                 result.append((name, self.path+f"#{anchor}",  '\n' + url + '\n' + ''.join(table_of_params)))
         # print(*result, sep='\n')
         return SearchResult(result)
+
+
+class JsDocSearcher(Searcher):
+    def __init__(self, path):
+        self.path = path
+
+    def search(self, query):
+        search_result = []
+        options = webdriver.FirefoxOptions(); options.add_argument('-headless')
+        driver = webdriver.Firefox(options=options)
+        driver.get(self.path)
+
+        input_elem = driver.find_element(By.ID, "search-input")
+        button_elem = driver.find_element(By.ID, "search-submit")
+
+        print(input_elem)
+
+        input_elem.send_keys(query)
+        button_elem.click()
+
+        search_result_list = driver.find_elements(By.TAG_NAME, "a")
+        for elem in search_result_list:
+            if elem.text == "" or\
+                elem.find_element(By.XPATH, '..').tag_name != "li" or\
+                elem.text == "Namespaces" or\
+                elem.text == "Classes" or\
+                elem.find_element(By.XPATH, '..').find_element(By.XPATH, '..').location.get('y') < 10:
+                    continue
+            search_result.append((elem.text, elem.get_attribute('href'), ""))
+
+        return SearchResult(search_result)
+                
+
+class MattermostHistorySearcher(Searcher):
+    def __init__(self, token, url, team_id):
+        self.token = token
+        self.url = url
+        self.team_id = team_id
+        self.headers = {
+            'Content-Type': 'application/json',
+            'Authorization': f"Bearer {self.token}"
+        }
+
+    def get_channels_list(self):
+        request_channels = requests.get(self.url + '/api/v4/channels', headers=self.headers).json()
+        list_of_channels = {}
+        for elem in request_channels:
+            list_of_channels[elem["id"]] = (elem["name"], elem["display_name"])
+        return list_of_channels
+
+    def search(self, query):
+        json_params = { 'terms': query, 'is_or_search': True }
+        
+        response = requests.post(
+            self.url + f'/api/v4/teams/{self.team_id}/posts/search',
+            headers=self.headers, json=json_params)
+
+        posts = sorted(response.json()['posts'].values(), key=itemgetter('create_at'))
+        list_channels = self.get_channels_list()
+        search_result = []
+
+        for item in posts:
+            unix_timestamp = int(item['create_at'])//1000
+            date_string = datetime.fromtimestamp(unix_timestamp).strftime('%d-%m-%Y')
+            channel_name = list_channels.get(item['channel_id'], ('', 'Неизвестно'))
+            
+            search_result.append((
+                item['message'],
+                self.url + f'/main/channels/{channel_name[0]}#post_{item["id"]}',
+                date_string + ": " +
+                f"Расположение сообщения: {channel_name[1]}"
+            ))
+
+        return SearchResult(search_result)
+
